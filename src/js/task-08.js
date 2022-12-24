95e2d82efc1ddf6ca65f07884ee1f325132c6070
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 16:43:15 +0200

    жалка попитка

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,8 @@
+const inputForm = document.getElementsByClassName("login-form");
 
+const emailForm = document.getElementsByName("email");
+
+const submitButton = document.querySelector('button[type="submit"]');
+
+
+console.log(inputForm);
