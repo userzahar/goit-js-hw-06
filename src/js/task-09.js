8594250c05fc2244d2e2c559ce7926f09e351f00
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 18:32:35 +0200

    9 готово

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widgetButton = document.querySelector('.change-color');
+const widgetTextColor = document.querySelector('.color');
+const body = document.querySelector('body');
+
+widgetButton.addEventListener('click', changeBodyBackgroundColor);
+
+function changeBodyBackgroundColor(event) {
+  widgetTextColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = widgetTextColor.textContent;
+}
+
+
+
