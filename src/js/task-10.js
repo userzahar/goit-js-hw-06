fb82ad004fe18faf2133fa1a90bd4bbc8e558039
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 19:00:13 +0200

    10 пробую

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxMother = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  for (let i = 0; i <= amount; i += 1){
+    const newBox = document.createElement('div');
+    console.log("🚀 ~ file: task-10.js:10 ~ createBoxes ~ newBox", newBox)
+    
+    const arrayBox = [];
+    arrayBox.push(newBox);
+    console.log(arrayBox);
+    // boxMother.append(newBox)
+    newBox.style.width = "30px";
+    newBox.style.height = "30px";
+    newBox.style.backgroundColor = "#ffa321";
+  }
+}
+createBoxes(2)
+
