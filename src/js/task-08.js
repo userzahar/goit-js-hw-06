4bbec0a91e4fd5ba7893c54161ad48cc038b6f66
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 17:53:55 +0200

    лишні консоль лох

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,14 +1,10 @@
 const inputForm =  document.querySelector(".login-form");
-console.log("1", inputForm);
 
 const emailForm = document.querySelector('input[type="email"]');
-console.log("2", emailForm);
 
 const passwordForm = document.querySelector('input[type="password"]');
-console.log("3", passwordForm);
 
 const submitButton = document.querySelector('button[type="submit"]');
-console.log("4", submitButton);
 
 inputForm.addEventListener('submit', onButtonSubmitForm );
 
