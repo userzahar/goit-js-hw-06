2ccdcb798acbe9c742459d14cf11181b2858efb5
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 16:10:29 +0200

    7 готово

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,9 +2,8 @@ const inputFontSizeControl = document.querySelector("input#font-size-control");
 const goodText = document.querySelector("span#text");
 
 inputFontSizeControl.addEventListener('input', onDragMakeFontSize);
+
 function onDragMakeFontSize(event) {
     const value = event.currentTarget.value;
-    goodText.style.fontSize = value;
-    console.log("🚀 ", goodText);
-
+    goodText.style.fontSize = `${value}px`;
 }
