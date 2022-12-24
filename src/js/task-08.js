9137eda11cb1ea77360cd0b37ab464553c431127
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-24 17:53:10 +0200

    виконав 8 завд

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,8 +1,27 @@
-const inputForm = document.getElementsByClassName("login-form");
+const inputForm =  document.querySelector(".login-form");
+console.log("1", inputForm);
 
-const emailForm = document.getElementsByName("email");
+const emailForm = document.querySelector('input[type="email"]');
+console.log("2", emailForm);
+
+const passwordForm = document.querySelector('input[type="password"]');
+console.log("3", passwordForm);
 
 const submitButton = document.querySelector('button[type="submit"]');
+console.log("4", submitButton);
 
+inputForm.addEventListener('submit', onButtonSubmitForm );
 
-console.log(inputForm);
+function onButtonSubmitForm(event) {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === '' || password.value === '') alert('Всі поля повинні бути заповнені')
+    else {
+        const userRegister = {
+        email: email.value,
+        password: password.value,
+        } 
+        console.log(userRegister)
+    }
+    inputForm.reset();
+}
